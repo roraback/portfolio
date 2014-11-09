f1e15425ff22e42613102cc038a77b103767acd9
Kenneth Roraback <kroraback@gmail.com>
2014-11-09 13:10:37 -0500

    [KLR] Improved Admin to add project in one screen

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from projects.models import Image, Addendum, Project, Category
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+class AddendumInline(admin.TabularInline):
+    model = Addendum
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ImageInline, AddendumInline]
+
 admin.site.register(Image)
 admin.site.register(Addendum)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Category)
