2ed797ae02e870cd81292e5cbfd9f23dfa46801b
Kenneth Roraback <kroraback@gmail.com>
2014-11-28 20:12:11 -0500

    [KLR] Added media url/root to settings file

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -88,4 +88,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+MEDIA_DIRECTORY = '/media/'
+STATIC_DIRECTORY = '/static/'
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
